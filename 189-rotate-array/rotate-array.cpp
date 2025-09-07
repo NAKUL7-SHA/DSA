cef478d73ca172754c44dabb8388adc6fdfcbb15
nakul <NAKULJAVA566@GMAIL.COM>
2025-09-07 07:51:33 +0530

    Time: 0 ms (100.00%) | Memory: 29.6 MB (38.25%) - LeetSync

---
+++ 189-rotate-array/rotate-array.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+#include<iostream>
+class Solution {
+public:
+    void rotate(vector<int>& arr, int k) {
+        int n=arr.size();
+        k=k%n;
+          reverse(arr.begin(),arr.end());
+  reverse(arr.begin(),arr.begin()+k);
+  reverse(arr.begin()+k,arr.end());
+
+  
+        
+    }
+};
