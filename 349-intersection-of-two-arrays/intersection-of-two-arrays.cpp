6db08f06e63a4cc91e4dfbcf5f17e43af743c2e5
nakul <NAKULJAVA566@GMAIL.COM>
2025-09-07 12:42:55 +0530

    Time: 0 ms (100.00%) | Memory: 13.8 MB (89.98%) - LeetSync

---
+++ 349-intersection-of-two-arrays/intersection-of-two-arrays.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    vector<int> intersection(vector<int>&a, vector<int>&b) {
+        int n1=a.size();
+        int n2=b.size();
+        int i=0;
+        int j=0;
+        vector<int> ans;
+         sort(a.begin(), a.end());
+        sort(b.begin(), b.end());
+        while(i<n1 && j<n2){
+            if(a[i]<b[j]){
+                i++;
+            }
+            else if(b[j]<a[i]){
+                j++;
+                
+
+            }
+            else{
+                if (ans.empty() || ans.back() != a[i]) {
+                    ans.push_back(a[i]);
+                }
+                i++;
+                j++;
+           
+            }
+
+           
+        }
+        return ans;
+
+        
+    }
+};
