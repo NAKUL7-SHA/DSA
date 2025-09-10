026e47f8b419830a918630ec0aa96552d0125e50
nakul <NAKULJAVA566@GMAIL.COM>
2025-09-10 22:50:32 +0530

    Time: 10 ms (36.70%) | Memory: 128.1 MB (94.45%) - LeetSync

--- 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.cpp
+++ 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.cpp
@@ -2,26 +2,19 @@ class Solution {
 public:
     vector<int> rearrangeArray(vector<int>& arr) {
         int n=arr.size();
-        vector<int>pos;
-        vector<int>neg;
-       for(int i=0;i<n;i++){
-        if(arr[i]>0){
-             pos.push_back(arr[i]);
-        }
-        else{
-             neg.push_back(arr[i]);
-        }
-
-       }
-       for(int i=0;i<n/2;i++){
-        arr[2*i]=pos[i];
-        arr[2*i+1]=neg[i];
-
-       }
-
-        
-
-
-        return arr;
+        vector<int> ans(n,0);
+// arr=[3,-2,1,-5,2,-4] posstive value 0 se start ho rhi hai negative 1 se
+int posind=0,negind=1;
+for(int i=0;i<n;i++){
+    if(arr[i]>0){
+        ans[posind]=arr[i];
+        posind+=2;
+    }
+    else{
+        ans[negind]=arr[i];
+        negind+=2;
+    }
+}
+return ans;
     }
 };
