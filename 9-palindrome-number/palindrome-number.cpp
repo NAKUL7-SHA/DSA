5c172b9ed78920d4047b9a153a9cf3486f564676
nakul <NAKULJAVA566@GMAIL.COM>
2025-10-01 22:31:40 +0530

    Time: 3 ms (45.43%) | Memory: 8.5 MB (91.94%) - LeetSync

--- 9-palindrome-number/palindrome-number.cpp
+++ 9-palindrome-number/palindrome-number.cpp
@@ -1,19 +1,13 @@
 class Solution {
 public:
     bool isPalindrome(int n) {
-        long long rev=0;
-        int orignal=n;
-        if (n<0) return false;
-        while(n>0){
-        int rem=n%10;
-         rev=rev*10+rem;
-           n=n/10;
-        
-        } 
-        if(rev==orignal) return true;
-        else return false; 
+        if (n < 0) return false;
 
-        
-        
+        long long rev = 0, original = n;
+        while (n > 0) {
+            rev = rev * 10 + (n % 10);
+            n /= 10;
+        }
+        return rev == original;
     }
 };
