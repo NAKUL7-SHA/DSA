fdfee26ddc0447147d2fabe270f0672430dc33f8
nakul <NAKULJAVA566@GMAIL.COM>
2026-02-04 03:17:43 +0530

    Time: 0 ms (100.00%) | Memory: 11.8 MB (11.21%) - LeetSync

--- 75-sort-colors/sort-colors.cpp
+++ 75-sort-colors/sort-colors.cpp
@@ -1,26 +1,33 @@
 class Solution {
 public:
-    void sortColors(vector<int>& a) {
-        int n=a.size();
-        
-        int countzero=0,countone=0,counttwo=0;
-        for(int i=0;i<n;i++){
-            if(a[i]==0) countzero++;
-             else if(a[i]==1) countone++;
-              else counttwo++;
-
-        }
-        for(int i=0;i<countzero;i++){
-            a[i]=0;
-        }
-         for(int i=countzero;i<countzero+countone;i++){
-            a[i]=1;
-        }
-         for(int i=countzero+countone;i<n;i++){
-            a[i]=2;
-        }
-
-
-    
+    void sortColors(vector<int>& nums) {
+    int n=nums.size();
+    int arr[n];
+    int count0=0;
+    int count1=0;
+    int count2=0;
+ for(int i=0;i<n;i++){
+    if(nums[i]==0){
+        count0++;
+    }
+     else if(nums[i]==1) {
+          count1++;    
+    }
+   else{
+    count2++;
+   }
+    }
+    for(int i=0;i<count0;i++){
+        arr[i]=0;
+    }
+     for(int i=count0;i<count0+count1;i++){
+        arr[i]=1;
+    }
+     for(int i=count0+count1;i<n;i++){
+        arr[i]=2;
+    }
+      for (int i = 0; i < n; i++){
+            nums[i] = arr[i];
+      }
     }
 };
