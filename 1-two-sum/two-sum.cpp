606c04920824239b2bf0093e28a6e086cac8fce4
nakul <NAKULJAVA566@GMAIL.COM>
2026-02-04 02:30:13 +0530

    Time: 3 ms (68.05%) | Memory: 14.7 MB (60.82%) - LeetSync

--- 1-two-sum/two-sum.cpp
+++ 1-two-sum/two-sum.cpp
@@ -1,19 +1,35 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        int n=nums.size();
-        map<int ,int> mpp;
-        for(int i=0;i<n;i++){
-            int a=nums[i];
-            int remaining=target-a;
-            if(mpp.find(remaining)!=mpp.end()){
-                return {mpp[remaining],i};
-
-            }
-            mpp[a]=i;
-
-        }
-        return {-1,-1};
+        int n = nums.size();
         
+        // 1. Store value and original index together
+        vector<pair<int, int>> indexed_nums;
+        for (int i = 0; i < n; i++) {
+            indexed_nums.push_back({nums[i], i});
+        }
+        
+        // 2. Sort the pairs (this sorts by the value, but keeps the index with it)
+        sort(indexed_nums.begin(), indexed_nums.end());
+        
+        int left = 0;
+        int right = n - 1;
+
+        while (left < right) {
+            // 3. Access the value using .first
+            int sum = indexed_nums[left].first + indexed_nums[right].first;
+            
+            if (sum == target) {
+                // 4. Return the original indices using .second
+                return {indexed_nums[left].second, indexed_nums[right].second};
+            }
+            else if (sum < target) {
+                left++;
+            }
+            else {
+                right--;
+            }
+        }
+        return {};
     }
 };
