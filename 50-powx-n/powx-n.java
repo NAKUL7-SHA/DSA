79bd4f638e790b5aecdb93df23d0d7d5fcb83225
nakul <NAKULJAVA566@GMAIL.COM>
2026-03-30 19:56:52 +0530

    Time: 0 ms (100.00%) | Memory: 47.2 MB (95.60%) - LeetSync

---
+++ 50-powx-n/powx-n.java
@@ -0,0 +1,21 @@
+class Solution {
+    public double myPow(double x, int n) {
+          if (n == 0) return 1;
+
+        long N = n;  // ✅ correct in Java
+
+        if (N < 0) {
+            x = 1 / x;
+            N = -N;
+        }
+
+        double half = myPow(x, (int)(N / 2));
+
+        if (N % 2 == 0) {
+            return half * half;
+        } else {
+            return x * half * half;
+        }
+        
+    }
+}
